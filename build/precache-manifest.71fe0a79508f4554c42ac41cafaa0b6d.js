c30d375e0b8ba0443703744889f7b41225ddb4a0
Samarth Ghadi <samarthghadi@gmail.com>
2022-12-04 20:22:49 -0500

    second commit

---
+++ build/precache-manifest.71fe0a79508f4554c42ac41cafaa0b6d.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67bc03f73b3dbcd234065a7746811f20",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6dcf3cdfcb338ca8f4f2",
+    "url": "/static/js/2.d8e56f8d.chunk.js"
+  },
+  {
+    "revision": "9ee332b879751bee1e52",
+    "url": "/static/js/main.a23782ac.chunk.js"
+  },
+  {
+    "revision": "641bb0516db3d047d70b",
+    "url": "/static/js/runtime-main.be65498d.js"
+  }
+]);
